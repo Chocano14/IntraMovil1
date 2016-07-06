0d7900456c82cea4d2b738982cce595aa2f181c0
pablo <chocano14@outlook.com>
2016-07-06 15:27:08 -0400

    commint login

--- app/src/main/java/com/example/hgmovil/intramovil/LoginActivity.java
+++ app/src/main/java/com/example/hgmovil/intramovil/LoginActivity.java
@@ -2,23 +2,41 @@ package com.example.hgmovil.intramovil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity
 {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState)
-    {
+    {super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        Button boton = (Button) findViewById(R.id.btnIniciar);
+        final EditText Contraseña = (EditText) findViewById(R.id.txtContra);
+        final EditText Rut = (EditText)findViewById(R.id.txtRut);
+
+        boton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                String usuario=Rut.getText().toString();
+                String passw=Contraseña.getText().toString();
 
 
 
 
 
+
+
+            }
+        });
     }
-
-
-
-    }
+}
 
 
 
---
+++ app/src/main/java/com/example/hgmovil/intramovil/modeloDAO/AlumnoDAO.java
@@ -0,0 +1,20 @@
+package com.example.hgmovil.intramovil.modeloDAO;
+
+import android.content.Context;
+
+import com.example.hgmovil.intramovil.sqlite.BDIntraMovil;
+
+/**
+ * Created by pablo on 06-07-2016.
+ */
+public class AlumnoDAO extends BDIntraMovil
+{
+    public AlumnoDAO(Context context)
+    {
+        super(context);
+    }
+
+
+
+}
+
